be2f91ccb10c2e6100aba1f285218ad039c3a8d1
Emilia990810 <haufikuemiliasnk@gmail.com>
2025-03-19 13:56:14 +0200

    first

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
@@ -1,6 +1,5 @@
 package jetbrains.kotlin.course.alias.card
 
-import jetbrains.kotlin.course.alias.util.Identifier
 import jetbrains.kotlin.course.alias.util.IdentifierFactory
 
 class CardService {
@@ -8,11 +7,17 @@ class CardService {
 
     companion object {
         const val WORDS_IN_CARD = 4
+        val words = listOf(
+            "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew",
+            "kiwi", "lemon", "mango", "nectarine", "orange", "papaya", "quince", "raspberry",
+            "strawberry", "tangerine", "orange", "vanilla", "watermelon", "xylophone", "yam", "zucchini"
+        )
         val cardsAmount = words.size / WORDS_IN_CARD
     }
 
     val cards: List<Card> = generateCards()
 
+    @Suppress("UNUSED")
     private fun List<String>.toWords(): List<Word> = this.map { Word(it) }
 
     private fun generateCards(): List<Card> {
